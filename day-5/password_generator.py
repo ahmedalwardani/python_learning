219326af8c6b35e0dbe645c268dd484de3cf032f
Ahmed Alwardani <aalwa049@uottawa.ca>
2023-08-23 21:29:04 -0400

    add password manager

--- day-5/password_generator.py
+++ day-5/password_generator.py
@@ -23,8 +23,10 @@ for number in range(1, nr_numbers + 1):
   
 print(f"Here is your password: {password}")
 
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+# Hard Level - Order of characters randomised:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
 
 password_list = list(password)
 random.shuffle(password_list)
